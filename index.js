cb1e114008b1d7ed848cf8d0f8309216007e3239
name <you@example.com>
2018-06-14 10:24:04 +0800

    改写了index.js

--- index.js
+++ index.js
@@ -2,12 +2,9 @@ var  oLi = document.getElementsByTagName("li")
 for(var i =0;i<oLi.length;i++){
 	oLi[i].index = i ;
 	oLi[i].onclick=function(){
-		for(var y =0;y<oLi.length;y++){
-			oLi[y].style.background='green';
+		for(var j=0;j<oLi.length;j++){
+			oLi[j].className = "";
 		}
-		oLi[index].style.background="red";
-		console.log(1)
-		console.log(2)
-		console.log(3)
+		oLi[this.index].className = "active"
 	}
 }
